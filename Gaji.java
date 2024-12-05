4ee399f4b887f691c68bd01e8b84940b500575a1
Dalfa Munawwarotul Mahmudah <dalfamunawwaroh@gmail.com>
2024-12-05 15:56:14 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,20 +1,49 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-public class Gaji{
+public class Gajii {
     public static int masaKerja;
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+
+        int jamLembur;
+        int gajiPokok = 5000000;
+        int lemburPokok = 40000;
+        int totalGaji;
+        int bonus = 0;
+
+
+        System.out.println("Masukan Masa Kerja: ");
         masaKerja = input.nextInt();
-        int jamLembur = input.nextInt();
+        System.out.println("Masukan Jam Lembur: ");
+        jamLembur = input.nextInt();
 
-        /* Pekerjaan anda mulai dari sini */
 
-        /* Pekerjaan anda berakhir sini */
+        if (masaKerja >= 5 && masaKerja <= 10) {
+            gajiPokok = 7500000;
+            lemburPokok = 50000;
+        } else if (masaKerja > 10) {
+            gajiPokok = 10000000;
+            lemburPokok = 75000;
+        }
 
-        System.out.println(Gaji.class.getDeclaredMethods().length);
-        System.out.println(Gaji.class.getDeclaredFields().length);
         
+        if (jamLembur > 0) {
+            bonus = lemburPokok * jamLembur;
+        }
+
+       
+        totalGaji = gajiPokok + bonus;
+
+        
+        System.out.println("Total Gaji: Rp. " + totalGaji);
+
+        
+        input.close();
+
+        
+        System.out.println(Gajii.class.getDeclaredMethods().length);
+        System.out.println(Gajii.class.getDeclaredFields().length);
     }
-    
 }
